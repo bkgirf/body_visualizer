efa62b3ddc84710ba6df4e81808abf80808d5399
bkgirf <blurry64+dev@gmail.com>
2025-03-31 11:13:56 +0000

    Modify setup.py to resolve outdated pyopengl dependency error

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         # 3D 렌더링
         "pyrender==0.1.43",
         "trimesh==3.9.29",
-        "PyOpenGL==3.1.0",
+        "PyOpenGL==3.1.4",
         "PyOpenGL_accelerate",
         
         # 데이터 처리
